14c3a6b85a1c6af0c5eb454d54bcf1151ac5913c
AdejumoOlanrewaju <olanrewajuadejumo56@gmail.com>
2024-04-24 11:21:38 -0700

    another non working branch

--- env/Scripts/deactivate.bat
+++ env/Scripts/deactivate.bat
@@ -1,23 +1,24 @@
-@set VIRTUAL_ENV=
-@set VIRTUAL_ENV_PROMPT=
+@echo off
 
-@REM Don't use () to avoid problems with them in %PATH%
-@if not defined _OLD_VIRTUAL_PROMPT @goto ENDIFVPROMPT
-    @set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
-    @set _OLD_VIRTUAL_PROMPT=
-:ENDIFVPROMPT
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+)
+set _OLD_VIRTUAL_PROMPT=
 
-@if not defined _OLD_VIRTUAL_PYTHONHOME @goto ENDIFVHOME
-    @set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
-    @set _OLD_VIRTUAL_PYTHONHOME=
-:ENDIFVHOME
+if defined _OLD_VIRTUAL_PYTHONHOME (
+    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
+    set _OLD_VIRTUAL_PYTHONHOME=
+)
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 @if not defined _OLD_VIRTUAL_PATH @goto ENDIFVPATH
     @set "PATH=%_OLD_VIRTUAL_PATH%"
     @set _OLD_VIRTUAL_PATH=
 :ENDIFVPATH
 =======
+=======
+>>>>>>> parent of c811851 (latest changes)
 if defined _OLD_VIRTUAL_PATH (
     set "PATH=%_OLD_VIRTUAL_PATH%"
 )
@@ -25,7 +26,12 @@ if defined _OLD_VIRTUAL_PATH (
 set _OLD_VIRTUAL_PATH=
 
 set VIRTUAL_ENV=
+<<<<<<< HEAD
 set VIRTUAL_ENV_PROMPT=
 
 :END
 >>>>>>> refs/remotes/origin/main
+=======
+
+:END
+>>>>>>> parent of c811851 (latest changes)
