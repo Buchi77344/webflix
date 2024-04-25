06ba61b7d771c4d49cbbeae9fc32f573350682b3
AdejumoOlanrewaju <olanrewajuadejumo56@gmail.com>
2024-04-25 01:21:10 -0700

    merge changes

--- base/static/js/script.js
+++ base/static/js/script.js
@@ -43,44 +43,44 @@ let movieTextEl = document.querySelector(".movie-text")
 let movieDesc = document.querySelector(".description")
 let videoContainer = document.querySelector(".video-card-container")
 
-async function getMovie(){
-    const API_KEY = "84cce1176ec500693be04f9ce60273de"
-    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=84cce1176ec500693be04f9ce60273de`)
-    .then(res => res.json())
-    .then(data => sendMovie(data))
-    .catch(err => console.log(err))
+// async function getMovie(){
+//     const API_KEY = "84cce1176ec500693be04f9ce60273de"
+//     fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=84cce1176ec500693be04f9ce60273de`)
+//     .then(res => res.json())
+//     .then(data => sendMovie(data))
+//     .catch(err => console.log(err))
 
-    // const res = await fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&${API_KEY}`)
-    // const data = await res.json()
-    // return  data
-}
+//     // const res = await fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&${API_KEY}`)
+//     // const data = await res.json()
+//     // return  data
+// }
 
-function sendMovie(data){
-    console.log(data)
-    let listMel = ''
-    for(let i = 0; i < data.results.length; i++){
-        listMel += `
+// function sendMovie(data){
+//     console.log(data)
+//     let listMel = ''
+//     for(let i = 0; i < data.results.length; i++){
+//         listMel += `
         
-            <a class = "video-link" href = "/">
-                <img class = "video-card" src = "" alt = "video image card"/>
-                <div class = "video-description">
-                    <h3>HD</h3>
+//             <a class = "video-link" href = "/">
+//                 <img class = "video-card" src = "" alt = "video image card"/>
+//                 <div class = "video-description">
+//                     <h3>HD</h3>
     
-                    <div class = "details">
-                        <p>${data.results[i].original_title}</p>
-                        <p>PG-13</p>
-                        <p>${data.results[i].vote_average}</p>
-                    </div>
-                </div>
-            </a>
-        `
+//                     <div class = "details">
+//                         <p>${data.results[i].original_title}</p>
+//                         <p>PG-13</p>
+//                         <p>${data.results[i].vote_average}</p>
+//                     </div>
+//                 </div>
+//             </a>
+//         `
     
-    }
-    videoContainer.innerHTML = listMel
-    console.log(movieDesc)
-}
+//     }
+//     videoContainer.innerHTML = listMel
+//     console.log(movieDesc)
+// }
 
-getMovie()
+// getMovie()
 
 
 let imgWidth
